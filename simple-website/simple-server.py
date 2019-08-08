2567fd2a044d4955a44b8d97944afeaacfbd361a
JuliaHT <julia.hamblin@code.berlin>
2019-08-08 17:33:10 +0200

    Update simple-server.py

--- simple-website/simple-server.py
+++ simple-website/simple-server.py
@@ -8,3 +8,4 @@ Handler = http.server.SimpleHTTPRequestHandler
 with socketserver.TCPServer(("127.0.0.1", PORT), Handler) as httpd:
     print("serving at port", PORT)
     httpd.serve_forever()
+    
